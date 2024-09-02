4c736cd08ca43ccbe39e2a2d901fb8145d55253f
cattuongtu <cattuongtu3@gmail.com>
2024-09-01 22:09:17 -0700

    Triggering deploy.

--- src/components/TimerTabs.tsx
+++ src/components/TimerTabs.tsx
@@ -23,7 +23,7 @@ export default function TimerTabs() {
           
         </TabList>
       </Box>
-      <TabPanel value="1">Item One</TabPanel>
+      <TabPanel value="1">Test Item</TabPanel>
       <TabPanel value="2">Item Two</TabPanel>
       <TabPanel value="3">Item Three</TabPanel>
       
